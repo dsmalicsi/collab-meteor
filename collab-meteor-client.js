39037d2b01d80aefa45dd3ef8e63391283282db9
Dario Anongba Varela <dario.anongba@gmail.com>
2017-04-06 13:18:26 +0200

    Dependencies bug

--- collab-meteor-client.js
+++ collab-meteor-client.js
@@ -3,7 +3,5 @@ import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
 checkNpmVersions({
   'react': '15.4.2',
   'react-dom': '15.4.2',
-  "react-router": "3.0.2",
-  "sharedb": "^1.0.0-beta.7",
-  "sharedb-string-binding": "^1.0.0"
+  "react-router": "3.0.2"
 }, 'danongba:meteor-sharedb');
